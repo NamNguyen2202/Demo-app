0b187fd15a5a9524a4575cb23acfdcfe0af70335
NamNguyen2202 <namnamm220201@gmail.com>
2024-04-09 16:59:54 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './component/client/home-page/home-page.component';
+import { LoginPageComponent } from './component/client/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component : LoginPageComponent},
+  {path: 'home-page', component : HomePageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './component/client/login-page/login-page.component';
+import { HomePageComponent } from './component/client/home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
